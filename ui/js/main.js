363333ce76d9f43c53e6a645fa46acf67875e7d2
ugwumadu116 <ugwumadu116@gmail.com>
2019-02-28 17:52:00 +0100

    [Feature #164307015] Add success message

--- ui/js/main.js
+++ ui/js/main.js
@@ -1,15 +1,32 @@
-const signup = document.querySelector(".signup");
-signup.addEventListener("click", callSignupForm);
-const closeForm = document.querySelector(".fa-times");
+// signup
+const signup = document.querySelector('.signup');
+signup.addEventListener('click', callSignupForm);
+const closeForm = document.querySelector('.fa-times');
 
-const register = document.getElementById("register");
+const register = document.getElementById('register');
 
-closeForm.addEventListener("click", closeSignup);
+closeForm.addEventListener('click', closeSignup);
 
 function callSignupForm() {
-    register.style.display = "block";
+    register.style.display = 'block';
 }
 
-function closeSignup(e) {
-    register.style.display = "none";
+function closeSignup() {
+    register.style.display = 'none';
+}
+
+/// success
+const signupForm = document.querySelector('#signupForm');
+const home = document.querySelector('#home');
+signupForm.addEventListener('submit', callSuccess);
+const body = document.querySelector('body');
+
+const successCard = document.querySelector('.successCard')
+
+function callSuccess(e) {
+    e.preventDefault();
+    register.style.opacity = "0.4";
+    home.style.opacity = "0.1";
+    successCard.style.display = "block";
+    e.preventDefault();
 }
