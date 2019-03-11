207789af089093ab2c0080d00904033d2e16f204
ugwumadu116 <ugwumadu116@gmail.com>
2019-03-11 03:28:56 +0100

    done with err

--- test/message.test.js
+++ test/message.test.js
@@ -68,7 +68,7 @@ describe('User Messages Endpoint Tests', () => {
         expect(res.body.status).to.eq(200);
         expect(res.body.message).to.not.equal(0);
       })
-      .catch(done());
+      .catch(err => done(err));
   });
   it('GET /messages/ - User has no recieved emails', (done) => {
     chai
