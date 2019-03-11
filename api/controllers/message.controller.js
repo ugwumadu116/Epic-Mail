235fd40a982c3975bad13fd0e0cfad00eb553c58
ugwumadu116 <ugwumadu116@gmail.com>
2019-03-11 04:33:48 +0100

    callback

--- api/controllers/message.controller.js
+++ api/controllers/message.controller.js
@@ -1,23 +1,19 @@
 import messageService from '../services/message.services';
 
 class MessageController {
-  static async getEmail(req, res) {
-    try {
-      const userMails = await messageService
-        .getUserEmails(req.userData.user.epicMail);
-      if (userMails.length === 0) {
-        throw new Error('No mail found');
-      }
-      return res.status(200).json({
-        status: 200,
-        message: userMails,
-      });
-    } catch (error) {
+  static getEmail(req, res) {
+    const userMails = messageService
+      .getUserEmails(req.userData.user.epicMail);
+    if (userMails.length === 0) {
       return res.status(404).json({
         status: 404,
-        message: error.message,
+        message: 'No mail found',
       });
     }
+    return res.status(200).json({
+      status: 200,
+      message: userMails,
+    });
   }
 
   static async getUnreadEmail(req, res) {
