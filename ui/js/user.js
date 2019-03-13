3f0d08e8bf72a5e649fd742347e728e372d1b43b
ugwumadu116 <ugwumadu116@gmail.com>
2019-03-13 23:50:54 +0100

    bug(ui): update ui functionalities
    - add send to group or single user
    - add notification menu icon a pop up
    
    [Delivers #164557830]

--- ui/js/user.js
+++ ui/js/user.js
@@ -10,19 +10,19 @@ const overlay2 = document.getElementById("overlay2");
 overlay2.addEventListener('click', closeOverlay2);
 
 function closeOverlay2() {
-    document.getElementById("overlay2").style.display = "none";
-    sidenav.style.width = '0';
-    closeNav.style.display = "none";
+  document.getElementById("overlay2").style.display = "none";
+  sidenav.style.width = '0';
+  closeNav.style.display = "none";
 }
 function callSidebar() {
-    document.getElementById("overlay2").style.display = "block";
-    sidenav.style.width = 'auto';
-    closeNav.style.display = "block";
+  document.getElementById("overlay2").style.display = "block";
+  sidenav.style.width = 'auto';
+  closeNav.style.display = "block";
 }
 function closeSidebar() {
-    document.getElementById("overlay2").style.display = "none";
-    sidenav.style.width = '0';
-    closeNav.style.display = "none";
+  document.getElementById("overlay2").style.display = "none";
+  sidenav.style.width = '0';
+  closeNav.style.display = "none";
 }
 // Open Email Message
 const messageCard = document.getElementsByClassName('messageCard');
@@ -31,18 +31,20 @@ const overlay = document.getElementById("overlay")
 overlay.addEventListener('click', closeOverlay)
 
 for (let i = 0; i < messageCard.length; i++) {
-    messageCard[i].addEventListener('click', openOverlay)
+  messageCard[i].addEventListener('click', openOverlay)
 }
 function openOverlay() {
-    document.getElementById("overlay").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
 }
 
 function closeOverlay(e) {
-    if (e.target === overlay) {
-        document.getElementById("overlay").style.display = "none";
-    }
+  if (e.target === overlay) {
+    document.getElementById("overlay").style.display = "none";
+  }
 }
+
 /// send draft
 document.getElementById("email").value = 'amaka@gmail.com'
 document.getElementById("subject").value = 'Git version control'
 const message = document.getElementById("message").value = 'this will be a git message'
+
