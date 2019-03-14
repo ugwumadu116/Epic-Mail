3b146302b91040ca45f6b508a5f9bc0a6163303b
ugwumadu116 <ugwumadu116@gmail.com>
2019-03-14 10:10:23 +0100

    chore(documentation): document api with swagger
    - add route for documentation
    
    [Delivers #164620261]

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 
 import userRouter from './routes/user.route';
 import messageRouter from './routes/message.route';
@@ -11,6 +13,7 @@ dotenv.config();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 3001;
 const prefix = '/api/v1';
