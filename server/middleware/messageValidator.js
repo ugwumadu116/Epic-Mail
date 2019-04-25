312aba0fe96a6de944da96285a3d10d3f3a005a2
ugwumadu116 <ugwumadu116@gmail.com>
2019-04-25 23:48:46 +0100

    test2

--- server/middleware/messageValidator.js
+++ server/middleware/messageValidator.js
@@ -1,26 +1,26 @@
-import Joi from 'joi';
+// import Joi from 'joi';
 
-class MessageMiddleware {
-  static async validatePostMail(req, res, next) {
-    try {
-      const schema = {
-        subject: Joi.string().required(),
-        message: Joi.string().required(),
-        receiverEmail: Joi.string()
-          .email()
-          .required(),
-      };
-      await Joi.validate(req.body, schema);
-      next();
-      return true;
-    } catch (err) {
-      return res.status(400).json({
-        status: 'error',
-        message: String(err.details[0].message),
-        type: 'validation',
-      });
-    }
-  }
-}
+// class MessageMiddleware {
+//   static async validatePostMail(req, res, next) {
+//     try {
+//       const schema = {
+//         subject: Joi.string().required(),
+//         message: Joi.string().required(),
+//         receiverEmail: Joi.string()
+//           .email()
+//           .required(),
+//       };
+//       await Joi.validate(req.body, schema);
+//       next();
+//       return true;
+//     } catch (err) {
+//       return res.status(400).json({
+//         status: 'error',
+//         message: String(err.details[0].message),
+//         type: 'validation',
+//       });
+//     }
+//   }
+// }
 
-export default MessageMiddleware;
+// export default MessageMiddleware;
--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -2,7 +2,6 @@ import express from 'express';
 import trimRequest from 'trim-request';
 import userAuth from '../middleware/userAuth';
 import MessageController from '../controllers/message.controller';
-import MessageMiddleware from '../middleware/messageValidator';
 import CheckMiddleware from '../middleware/helpers';
 
 const router = express.Router();
@@ -16,18 +15,17 @@ router.get('/:id',
   CheckMiddleware.checkIFMessageExist,
   MessageController.getAnEmail);
 
-router.post('/',
-  trimRequest.body,
-  userAuth.validate,
-  MessageMiddleware.validatePostMail,
-  CheckMiddleware.checkIfReceiverEmailExist,
-  MessageController.PostAnEmail);
+// router.post('/',
+//   trimRequest.body,
+//   userAuth.validate,
+//   CheckMiddleware.checkIfReceiverEmailExist,
+//   MessageController.PostAnEmail);
 
-router.delete('/:id',
-  userAuth.validate,
-  CheckMiddleware.paramIdValid,
-  CheckMiddleware.checkIFMessageExist,
-  CheckMiddleware.getUserId,
-  MessageController.deleteAnEmail);
+// router.delete('/:id',
+//   userAuth.validate,
+//   CheckMiddleware.paramIdValid,
+//   CheckMiddleware.checkIFMessageExist,
+//   CheckMiddleware.getUserId,
+//   MessageController.deleteAnEmail);
 
 export default router;
